8fe043280a5841c1928020b2fbd7e4a1788675e1
Emma Broman <emma95.broman@hotmail.com>
2018-12-18 11:54:34 +0100

    Import glm and update shader class to handle more uniforms

--- Shader.h
+++ Shader.h
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -28,6 +29,15 @@ public:
 	void setBool(const std::string &name, bool value) const;
 	void setInt(const std::string &name, int value) const;
 	void setFloat(const std::string &name, float value) const;
+	void setVec2(const std::string &name, const glm::vec2 &value) const;
+	void setVec2(const std::string &name, float x, float y) const;
+	void setVec3(const std::string &name, const glm::vec3 &value) const;
+	void setVec3(const std::string &name, float x, float y, float z) const;
+	void setVec4(const std::string &name, const glm::vec4 &value) const;
+	void setVec4(const std::string &name, float x, float y, float z, float w) const;
+	void setMat2(const std::string &name, const glm::mat2 &mat) const;
+	void setMat3(const std::string &name, const glm::mat3 &mat) const;
+	void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
 private:
 	void checkCompileErrors(unsigned int shader, std::string type);
